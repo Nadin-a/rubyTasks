1fe5ff311e1af8cd3cbf60befa6ed3527077b393
Nadin-A <nadezhda.trofimova@faceit-team.com>
2017-11-02 15:50:14 +0200

    finish task02

--- 02.rb
+++ 02.rb
@@ -2,13 +2,13 @@ puts 'Hello. Please enter a radius: '
 
 r = gets.to_i
 y = r
-r_in = r-0.4
-r_out = r+0.4
+r_in = r - 0.4
+r_out = r + 0.4
 
 until y < -r do
   x = -r
   until x >= r_out do
-    if x*x + y*y >= r_in*r_in && x*x + y*y <= r_out*r_out
+    if x * x + y * y >= r_in * r_in && x * x + y * y <= r_out * r_out
       p '*'
     else
       p ' '
