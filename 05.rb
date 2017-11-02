c93c8bfea5da39a8d7afe5b5114a85caddab0e7b
Nadin-A <nadezhda.trofimova@faceit-team.com>
2017-11-02 18:09:19 +0200

    better solution

--- 05.rb
+++ 05.rb
@@ -1,15 +1,10 @@
-array = [2, 5, 7, 6, 3, 8, 9, 100]
-
-p array
-min = array.min
-max = array.max
-
-
+array = [2, -500, 7, 6, 3, 125, 9]
 min_index = array.index(array.min)
 max_index = array.index(array.max)
+sub_arr = if min_index > max_index
+            array[(max_index + 1)...min_index]
+          else
+            array[(min_index + 1)...max_index]
+          end
 
-min_index += 1
-
-sub_arr = array[min_index...max_index].reverse
-
-p sub_arr
+p sub_arr.reverse
