265a3a12f1d1b1c22ab93cb65ce14a6ca34959e5
Nadin-A <nadezhda.trofimova@faceit-team.com>
2017-11-03 17:20:17 +0200

    finish task 16

--- 16.rb
+++ 16.rb
@@ -1,3 +1,9 @@
-mHash = {"Membran" => 168, "Fisher" => 186, "Warden" => 161, "Smith" => 176, "Lerman" => 174}
-p mHash.key(mHash.values.max)
+m_hash = [
+    { name: 'Membran', height: 168 },
+    { name: 'Fisher', height: 186 },
+    { name: 'Warden', height: 161 },
+    { name: 'Smith', height: 176 },
+    { name: 'Lerman', height: 174 }
+]
 
+p m_hash.max_by { |hash| hash[:height] }[:name]
