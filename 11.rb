d8072116ff232968f386d5100276e425aacccb19
Nadin-A <nadezhda.trofimova@faceit-team.com>
2017-11-03 11:34:26 +0200

    task 11

--- 11.rb
+++ 11.rb
@@ -1,10 +1,10 @@
-array = [0, -4, 2, 5, 3, 2, 7, 8, 9, 10, -9]
-indexes = []
+array = [-7, -4, 2, 5, 3, -2, 7, 8, 9, 10, -9]
 
-array.each do |i|
-  if i > 0
-    indexes.push(array.index(i))
+array.each do |elem|
+  if elem > 0
+    array.insert(array.index(elem)+1, 0)
   end
 end
 
-p indexes
+
+
