5a141c4afe9dc527ff782a27fbf0e0e89a7d238d
Nadin-A <nadezhda.trofimova@faceit-team.com>
2017-11-03 12:18:07 +0200

    finish task 11

--- 11.rb
+++ 11.rb
@@ -1,10 +1,7 @@
 array = [-7, -4, 2, 5, 3, -2, 7, 8, 9, 10, -9]
-
+new_arr = []
 array.each do |elem|
-  if elem > 0
-    array.insert(array.index(elem)+1, 0)
-  end
+  new_arr.push(elem)
+  new_arr.push(0) if elem.positive?
 end
-
-
-
+p new_arr
