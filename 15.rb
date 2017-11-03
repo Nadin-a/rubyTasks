db3ae48f36da1deaa08ae8bf9d259596f19cb039
Nadin-A <nadezhda.trofimova@faceit-team.com>
2017-11-03 17:20:11 +0200

    finish task 15

--- 15.rb
+++ 15.rb
@@ -1,8 +1,3 @@
-arr_height = [168, 186, 161, 176, 174]
-arr_surname = %w[ Membran Fisher Warden Smith Lerman]
-arr_height.each_with_index do |height, i|
-  arr_surname.each_with_index do |surname, j|
-    p surname if i == j && height == arr_height.max
-  end
-end
-
+heights = [168, 186, 161, 176, 174]
+names = %w[Membran Fisher Warden Smith Lerman]
+p names[heights.index(heights.max)]
