a31a50d9d425d354ad5128e37adaf4c6ed8de062
Nadin-A <nadezhda.trofimova@faceit-team.com>
2017-11-06 18:08:22 +0200

    finish task23

---
+++ 23.rb
@@ -0,0 +1,15 @@
+def formatize(str, type)
+  str = str.split
+  case type
+  when :camel
+    str.map(&:capitalize).join('')
+  when :underscore
+    str.map(&:downcase).join('_')
+  when :css
+    str.map(&:downcase).join('-')
+  end
+end
+
+p formatize('i love ruby', :camel)
+p formatize('i love ruby', :underscore)
+p formatize('i love ruby', :css)
