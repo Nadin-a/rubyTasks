8fca50d8a54b12fdba67071e3b0a417d6636f4d1
Nadin-A <nadezhda.trofimova@faceit-team.com>
2017-11-06 11:24:21 +0200

    finish task 17

--- 17.rb
+++ 17.rb
@@ -1,5 +1,7 @@
 arr = [3, -6, 3, -5, 6, -4]
-if arr[0..-2].map.with_index {|item, index| item.positive? && arr[index + 1].negative? || item.negative? && arr[index + 1].positive?}.include? false
+if arr[0..-2].map.with_index {
+    |item, index| item.positive? && arr[index + 1].negative? || item.negative? && arr[index + 1].positive?}
+       .include? false
   p 'Не чередуются'
 else
   p 'Чередуются'
