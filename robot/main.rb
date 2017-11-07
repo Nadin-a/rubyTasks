d988a1930b66498ef175e0838bef995c8a51145b
Nadin-A <nadezhda.trofimova@faceit-team.com>
2017-11-07 19:13:44 +0200

    comment cycle

--- robot/main.rb
+++ robot/main.rb
@@ -5,31 +5,33 @@ robot = Robot.new
 field = Field.new
 
 
-puts 'Hello. Enter your command'
+field.fill_field
 
-puts 'Place the robot! (PLACE X Y)'
-command = 'PLACE 0 1' #gets
-robot.place(command)
 
-puts 'Specify direction (NORTH, SOUTH, EAST, WEST)'
-command = gets
-command.strip!
-direction_of_robot = robot.choose_direction(command)
 
-while true
-  puts 'Enter a command!'
-  command = gets
-  command.strip!
-  p robot.coord
-  case command
-    when 'MOVE'
-      p 'im move'
-      robot.move
-    when 'REPORT'
-      p 'im report'
-      robot.report
-    else
-      direction_of_robot = robot.choose_direction(command)
-  end
-  field.show_field(robot.coord, direction_of_robot)
-end
+# puts 'Hello. Enter your command'
+#
+# puts 'Place the robot! (PLACE X Y)'
+# command = 'PLACE 0 1' #gets
+# robot.place(command)
+#
+# puts 'Specify direction (NORTH, SOUTH, EAST, WEST)'
+# command = gets
+# command.strip!
+# direction_of_robot = robot.choose_direction(command)
+#
+# while command != 'END'
+#   puts 'Enter a command!'
+#   command = gets
+#   command.strip!
+#   case command
+#     when 'MOVE'
+#       robot.move(field)
+#     when 'REPORT'
+#       robot.report
+#     else
+#       direction_of_robot = robot.choose_direction(command)
+#   end
+#   field.show_field(robot.coord, direction_of_robot)
+# end
+#
