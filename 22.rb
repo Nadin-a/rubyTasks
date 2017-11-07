90f16ba52545ddf9bfa0d19c057481ddec9b58e0
Nadin-A <nadezhda.trofimova@faceit-team.com>
2017-11-07 13:16:59 +0200

    finish 22

--- 22.rb
+++ 22.rb
@@ -1,12 +1,48 @@
-hash = {key1: {key89: 454545454}, key2: {}, key3: {key4: 'str', key5: 'str2', key6: {key7: {key8: 1, key9: [2]}}}}
+hash = {
+    key1:
+        {
+            key2: 454545454, key3: 19, key4:
+            {
+                key5: 'a', key6: 'b', key7: 'c', key8:
+                {
+                    key200: { key300:'hfkdjghdfkjg' }, key9: 'abc'
+                }
+            }
+        },
+    key10: {},
+    key11:
+        {
+            key12: 'str', key13: 'str2', key14:
+            {
+                key7: {key15: 1, key16: [2], key17:
+                    {
+                        key18: 11111, key19: 2222, key20:
+                        {
+                            key21: 2000, key22: 220000, key23:
+                            {
+                                key24: '555', key25:
+                                {
+                                    key26: '888', key27:
+                                    {
+                                        key28: '!!!!', key29: { key100: '100'},
+                                        key30: { key31: 'END' }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+}
 
 def find_by_key(hash, key)
   if hash.has_key?(key)
     p hash[key]
   else
-    find_by_key(hash[hash.keys.last], key)
+    hash.select {|k| find_by_key(hash[k], key) if hash[k].is_a? Hash}
   end
 end
+find_by_key(hash, :key300)
 
-find_by_key(hash, :key89)
 
