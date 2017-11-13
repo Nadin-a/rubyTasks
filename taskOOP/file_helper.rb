0f2a9d643c4483bfc7d15faf91033aad480ffdf5
Nadin-A <nadezhda.trofimova@faceit-team.com>
2017-11-13 16:21:34 +0200

    finish task with file helper

---
+++ taskOOP/file_helper.rb
@@ -0,0 +1,54 @@
+require './fixed_employee'
+require './hourly_employee'
+
+class FileHelper
+
+  def selecting_data_source
+    choice = 0
+    until choice == 1 || choice == 2
+      p '1) Select file 2) Use existing data'
+      choice = gets.to_i
+      empl_list =
+          case choice
+            when 1
+              search_file_with_list
+            when 2
+              [
+                  FixedEmployee.new('Jane', 8000), HourlyEmployee.new('Ron', 45),
+                  FixedEmployee.new('Stan', 8700), HourlyEmployee.new('Beverly', 70),
+                  HourlyEmployee.new('Jim', 35), FixedEmployee.new('Iren', 6750),
+                  FixedEmployee.new('Bill', 8700), HourlyEmployee.new('Susan', 60),
+                  HourlyEmployee.new('John', 45), FixedEmployee.new('Kate', 7500)
+              ]
+          end
+    end
+    empl_list
+  end
+
+  def selecting_file_for_writing(empl_list)
+    p 'Specify file for writing'
+    path = gets.strip
+    File.open(path, 'w') {|file| file.write Marshal.dump(empl_list)}
+  end
+
+
+  private
+
+  def search_file_with_list
+    p 'Enter path to file for reading data'
+    path = gets.strip
+    if File.exist? path
+      data_from_file = File.read(path)
+      begin
+        Marshal.load(data_from_file)
+      rescue => ex
+        p 'File doesn`t contain data'
+        search_file_with_list
+      end
+    else
+      p 'File doesn`t exist'
+      search_file_with_list
+    end
+  end
+
+end
--- taskOOP/main.rb
+++ taskOOP/main.rb
@@ -1,45 +1,20 @@
-require './fixed_employee'
-require './hourly_employee'
+require './file_helper'
 
-# empl_list = [
-#     FixedEmployee.new('Jane', 8000), HourlyEmployee.new('Ron', 45),
-#     FixedEmployee.new('Stan', 8700), HourlyEmployee.new('Beverly', 70),
-#     HourlyEmployee.new('Jim', 35), FixedEmployee.new('Iren', 6750),
-#     FixedEmployee.new('Bill', 8700), HourlyEmployee.new('Susan', 60),
-#     HourlyEmployee.new('John', 45), FixedEmployee.new('Kate', 7500)
-# ]
+file_helper = FileHelper.new
 
-def search_file_with_list
-  p 'Enter path to file '
-  path = gets.strip
-  if File.exist? path
-    data_from_file = File.read(path)
-    begin
-      Marshal.load(data_from_file)
-    rescue => ex
-      puts "#{ex.class}: #{ex.message}"
-      search_file_with_list
-    end
-  else
-    p 'File doesn`t exist'
-    search_file_with_list
-  end
-end
+empl_list = file_helper.selecting_data_source
 
-empl_list = search_file_with_list
-
+p 'List:'
+empl_list.each {|item| p item.to_s}
 
 p 'Sorted list:'
-sorted_list = empl_list.sort_by { |emp| [-emp.salary, emp.name] }.each { |emp| p emp.to_s }
+sorted_list = empl_list.sort_by {|emp| [-emp.salary, emp.name]}.each {|emp| p emp.to_s}
 
 p 'First five names:'
-sorted_list.first(5).each { |emp| p emp.name }
+sorted_list.first(5).each {|emp| p emp.name}
 
 p 'Last 3 id:'
-sorted_list.last(3).each { |emp| p emp.id }
+sorted_list.last(3).each {|emp| p emp.id}
 
-p 'Select file for writing:'
-path = gets.strip
-File.open(path, 'w') {|file| file.write Marshal.dump(empl_list)}
-
+file_helper.selecting_file_for_writing(empl_list)
 
